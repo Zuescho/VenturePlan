067e9c0a984fa61f93c3609ad837b9c872684bb5
David <44669930+dphennessy@users.noreply.github.com>
2021-07-08 15:13:37 +0100

    Adding "Emptied Mug" (332) and "Tainted Bite" (348)
    https://www.wowhead.com/adventure-combatant-ability/emptied-mug-332
    https://www.wowhead.com/adventure-combatant-ability/tainted-bite-348 (note that there's two tainted bites in wowhead db but the one with 348 as ID is the one that I found, dunno if the other is used)
    I don't have a screenshot for tainted bite since I had made it before finding this repo, but here's a screenshot for Emptied Mug https://i.imgur.com/usvMpxG.png

--- vs-spells.lua
+++ vs-spells.lua
@@ -352,8 +352,8 @@ T.KnownSpells = {
 	[303]={type="nuke", target="enemy-back", damageATK=25},
 
 	--New values for 9.1
-	--Contributors: TekGoNos, Zhavvorsa, Gaboros, Sehra, Ostjaevel, Ashmelev
-	[305]={type="nuke", target="enemy-back", damageATK=120},
+	--Contributors: TekGoNos, Zhavvorsa, Gaboros, Sehra
+	[305]={type="nuke", target="enemy-back" ,damageATK=120},
 	[315]={type="aura", target=1, damageATK1=150, duration=2, modDamageDealt=-30},
 	[326]={type="nuke", target="cleave", damageATK=25},
 	[327]={type="aura", target="all-other-allies", duration=3, plusDamageDealtATK=20},
@@ -378,5 +378,9 @@ T.KnownSpells = {
 	[346]={type="aura", target=0, duration=2, damageATK1=30, modDamageDealt=-1},
 	[349]={type="nuke", target="all-enemies", damageATK=8},
 	[332]={type="nuke", target=1, damageATK=150},
-	[347]={type="nuke", target="cone", damageATK=204},
+	[348]={
+		{type="nuke", target=1, damageATK=120},
+		{type="aura", target=1, duration=3, modDamageTaken=24},
+	},
+
 }
